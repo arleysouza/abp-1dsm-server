0779f8cc0e6d5a89d79be17eacad6528bfdaf10f
arleysouza <arley.ferreira.souza@gmail.com>
2024-05-15 20:51:56 -0300

    codigo

--- src/controladores/bd.js
+++ src/controladores/bd.js
@@ -5,7 +5,6 @@ dotenv.config();
 
 // Configura o pool de conexão, passando um objeto de configuração 
 // contendo as informações necessárias para se conectar ao BD do PostgreSQL
-/*
 const pool = new Pool({
   user: process.env.BDUSUARIO,
   host: process.env.BDHOST,
@@ -13,17 +12,17 @@ const pool = new Pool({
   password: process.env.BDSENHA,
   port: process.env.BDPORTA
 });
-*/
+
 
 // Configura o pool de conexão, passando um objeto de configuração 
 // A URI de conexão precisa estar na variável de ambiente BDURI do arquivo .env
-const pool = new Pool({
+/*const pool = new Pool({
   connectionString: process.env.BDURI,
   ssl: {
     rejectUnauthorized: false // Ajuste necessário caso esteja utilizando SSL e seu ambiente requeira essa configuração
   }
 });
-
+*/
 // Exporta a variável pool
 module.exports = { pool };
 
